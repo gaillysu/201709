d3e22c274af6d653432b651b8351464416f80232
DESKTOP-300QVBO\DengGang <xiongcai@med-corp.net>
2017-03-19 17:09:41 +0800

    [updata database]

--- library/src/main/java/net/medcorp/library/worldclock/WorldClockDatabaseHelper.java
+++ library/src/main/java/net/medcorp/library/worldclock/WorldClockDatabaseHelper.java
@@ -8,6 +8,8 @@ import net.medcorp.library.worldclock.event.WorldClockInitializeEvent;
 
 import org.greenrobot.eventbus.EventBus;
 
+import java.util.List;
+
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
 import io.realm.RealmResults;
@@ -23,13 +25,14 @@ public class WorldClockDatabaseHelper {
     private final int TIMEZONE_VERSION;
     private final SharedPreferences pref;
     private Realm realm;
-    private final String REALM_NAME = "default.realm";
+    private final String REALM_NAME = "med_library.realm";
+    private RealmConfiguration mConfig;
 
 
     public WorldClockDatabaseHelper(Context context) {
-        RealmConfiguration config = new RealmConfiguration.Builder(context).build();
-        Realm.setDefaultConfiguration(config);
+        setDefaultRealmData();
         this.context = context;
+        realm = Realm.getInstance(mConfig);
         CITIES_VERSION = context.getResources().getInteger(R.integer.config_preferences_cities_db_version_current);
         TIMEZONE_VERSION = context.getResources().getInteger(R.integer.config_preferences_timezone_db_version_current);
         pref = context.getSharedPreferences(context.getString(R.string.config_preferences_world_clock_preferences), Context.MODE_PRIVATE);
@@ -41,7 +44,7 @@ public class WorldClockDatabaseHelper {
             bumpTimeZoneVersion();
             setDefaultRealmData();
         } else if (getCitiesVersion() < CITIES_VERSION || getTimeZoneVersion() < TIMEZONE_VERSION) {
-            realm = Realm.getDefaultInstance();
+            realm = Realm.getInstance(mConfig);
             RealmResults<TimeZone> oldTimezones = realm.where(TimeZone.class).findAll();
             RealmResults<City> oldCities = realm.where(City.class).findAll();
             oldTimezones.deleteAllFromRealm();
@@ -54,9 +57,9 @@ public class WorldClockDatabaseHelper {
 
     private void setDefaultRealmData() {
         EventBus.getDefault().post(new WorldClockInitializeEvent(WorldClockInitializeEvent.STATUS.STARTED));
-        RealmConfiguration config = new RealmConfiguration.Builder(context).name(REALM_NAME)
-                .assetFile(context, REALM_NAME).build();
-        Realm.setDefaultConfiguration(config);
+        mConfig = new RealmConfiguration.Builder().name(REALM_NAME)
+                .assetFile(REALM_NAME).modules(new WorldClockLibraryModule()).build();
+        Realm.setDefaultConfiguration(mConfig);
         EventBus.getDefault().post(new WorldClockInitializeEvent(WorldClockInitializeEvent.STATUS.FINISHED));
     }
 
@@ -78,5 +81,26 @@ public class WorldClockDatabaseHelper {
 
     private int getTimeZoneVersion() {
         return pref.getInt(context.getString(R.string.config_preferences_timezone_db_saved_version), 0);
+    }
+
+    public City get(String name) {
+        realm.beginTransaction();
+        City alarm = realm.where(City.class).equalTo("name", name).findFirst();
+        realm.commitTransaction();
+        return alarm;
+    }
+
+    public City get(int id){
+        realm.beginTransaction();
+        City city = realm.where(City.class).equalTo("id", id).findFirst();
+        realm.commitTransaction();
+        return city;
+    }
+
+    public List<City> getAll() {
+        realm.beginTransaction();
+        RealmResults<City> allAlarms = realm.where(City.class).findAllSorted("name");
+        realm.commitTransaction();
+        return allAlarms;
     }
 }
